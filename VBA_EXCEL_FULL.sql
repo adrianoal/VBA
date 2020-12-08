793287ab1e3ea5638129ffd887fa2a916c8c562e
Adriano Lima <adriano.lima85@yahoo.com.br>
2020-12-08 19:09:50 -0300

    Minha primeira aula de VBA

---
+++ VBA_EXCEL_FULL.sql
@@ -0,0 +1,103 @@
+--------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
+					-- MACROS VBA PARA EXCEL COMPLETO - CONTRUA 7 FERRAMENTAS --	
+--------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------
+Prof: João Paulo de Lima
+
+SEÇÃO 1:INTRODUÇÃO
+
+1.Overview do CURSOR
+
+
+--------------------------------------------------------------------------------------------------------------------
+2.Como fazer esse curso?
+
+
+--------------------------------------------------------------------------------------------------------------------
+3.Apostila Completa do curso
+
+--------------------------------------------------------------------------------------------------------------------
+4.Um breve recado e como tirar sua dúvidas
+
+--------------------------------------------------------------------------------------------------------------------
+SEÇÃO 2:AMBIENTENTAÇÃO E PRIMEIROS PASSOS
+
+5.Introução - Formas de gravar uma Macro
+
+
+ Dá para criar Macros diretamente no Excel sem utilizar o VBA.
+ 
+ 
+ * Para habilitar a guia de desenvolvedor
+ 
+	Arquivos
+		Opções
+			Personalizar Faixa de Opções
+				Desenvolvedor
+
+
+				
+--------------------------------------------------------------------------------------------------------------------
+6.Ambiente VBA
+
+ Obs.: Sempre salvar uma macro como --> Pastas de Trabalho Habilitada para Macro do Excel
+
+
+--------------------------------------------------------------------------------------------------------------------
+7.Estrutura VBA e variáveis
+
+ A primeira coisa para escrever um código em VBA é colocar um marcador, pra colocar o código dentro, fazendo 
+analogia com o BEGIN e END do SQL...
+
+--------------------------------------------------------------------------------------------------------------------
+8.Primeira Ferramenta - Hello World e MsgBox
+
+ Nessa aula vamos fazer o primeira macro completa, primeiro projeto.
+ 
+ MsgBox é uma caixa/popap que aparece pra gente com o valores que a gente quiser.
+
+--------------------------------------------------------------------------------------------------------------------
+SEÇÃO 3:FUNDAMENTOS - VALORES
+
+9.Cells.Value e ActiveCell
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
